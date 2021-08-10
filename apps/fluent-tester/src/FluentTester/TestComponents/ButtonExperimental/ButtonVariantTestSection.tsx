c8ae228ae4d1a2f349bb7e76aa2c70c324824280
Linda <t-lindaweng@microsoft.com>
2021-08-10 10:34:42 -0700

    Save resolved merge conflicts

--- apps/fluent-tester/src/FluentTester/TestComponents/ButtonExperimental/ButtonVariantTestSection.tsx
+++ apps/fluent-tester/src/FluentTester/TestComponents/ButtonExperimental/ButtonVariantTestSection.tsx
@@ -5,8 +5,7 @@ import { commonTestStyles, stackStyle } from '../Common/styles';
 import { SvgIconProps } from '@fluentui-react-native/icon';
 import TestSvg from './test.svg';
 
-<<<<<<< HEAD
-export const ButtonVariantTest: React.FunctionComponent<{}> = () => {
+export const ButtonVariantTest: React.FunctionComponent = () => {
   const shadowStyles = {
     // shadowOffset: {    // shadow shows up if applied here
     //   width: 4,
@@ -23,9 +22,6 @@ export const ButtonVariantTest: React.FunctionComponent<{}> = () => {
 
   const testImage = require('./icon_24x24.png');
   console.log(testImage);
-=======
-export const ButtonVariantTest: React.FunctionComponent = () => {
->>>>>>> fa65d1793da67e5ec7af1266f5cce78d7f56ea58
   return (
     <View style={[stackStyle, commonTestStyles.view]}>
       <Button content="Default" style={commonTestStyles.vmargin} />
